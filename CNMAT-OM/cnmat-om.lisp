1c07d7bd1da7b83ce93afae822e965ba2c2de2f0
mattschumaker <matt.schumaker@gmail.com>
2016-05-11 11:39:28 -0700

    Test Commit and Sum Lists
    Added the object sum-lists as a test commit

--- CNMAT-OM/cnmat-om.lisp
+++ CNMAT-OM/cnmat-om.lisp
@@ -8,12 +8,15 @@
    :get-combi 
    :get-rotations
    :canon-query 
-   :rotation-canon-query))
+   :rotation-canon-query
+   :sum-lists))
 
 (in-package :om)
 
 (compile&load (om-relative-path '("src") "rhythmic-frames"))
 (compile&load (om-relative-path '("src") "combinations"))
+(compile&load (om-relative-path '("src") "sum-lists"))
+
 
 (set-lib-release 0.1)
 
@@ -35,7 +38,7 @@
 (in-package :cnmat)
 
 (let ((this-lib (om::find-library "CNMAT-OM")))
-  (om::addgenfun2pack '(get-combi get-rotations canon-query) this-lib))
+  (om::addgenfun2pack '(get-combi get-rotations canon-query sum-lists) this-lib))
 
 ;;; evaluate this to generate the HTML reference
 ;;; (gen-lib-reference (find-library "CNMAT-OM"))
