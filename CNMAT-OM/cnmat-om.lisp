092ed0ee3599a77d69fa1d2d5721bb207876d382
mattschumaker <matt.schumaker@gmail.com>
2016-05-16 21:50:28 -0700

    Revert "Adding operations"
    This reverts commit 27ca75cca1cad3cb7a127060d7571b78226185da.

--- CNMAT-OM/cnmat-om.lisp
+++ CNMAT-OM/cnmat-om.lisp
@@ -10,13 +10,7 @@
    :canon-query 
    :rotation-canon-query
    :sum-lists
-   :get-index
-   :count-truncate
-   :cribs
-   :equal-probability
-   :pitch-inversion
-   :repeat-list
-   :retrograde-list))
+   :get-index))
 
 (in-package :om)
 
@@ -24,22 +18,10 @@
 (compile&load (om-relative-path '("src") "combinations"))
 (compile&load (om-relative-path '("src") "sum-lists"))
 (compile&load (om-relative-path '("src") "get-index"))
-(compile&load (om-relative-path '("src") "count-truncate"))
-(compile&load (om-relative-path '("src") "cribs"))
-(compile&load (om-relative-path '("src") "equal-probability"))
-(compile&load (om-relative-path '("src") "pitch-inversion"))
-(compile&load (om-relative-path '("src") "repeat-list"))
-(compile&load (om-relative-path '("src") "retrograde-list"))
 
 
 
-
-
-
-
-
-
-(set-lib-release 0.1 (om::find-library "CNMAT-OM"))
+(set-lib-release 0.1)
 
 (doc-library "CNMAT-OM contains compositional tools collected from CNMAT researh and projects.")
 
@@ -58,23 +40,8 @@
 
 (in-package :cnmat)
 
-;(let ((this-lib (om::find-library "CNMAT-OM"))
-;  (om::addgenfun2pack '(get-combi get-rotations canon-query sum-lists get-index) this-lib))
-
-; ("sub-pack-name" subpacke-lists class-list function-list class-alias-list)
-(om::fill-library  
- '((NIL nil (prf) (get-combi get-rotations canon-query) nil)
-   ("operations" nil (prf) (sum-lists get-index count-truncate cribs equal-probability pitch-inversion repeat-list retrograde-list) nil)
-   ("queries-database" nil (prf) (get-combi get-rotations canon-query) nil)
-
-   
-   ;;use lines below for sub-sub menus, if needed
-   ;;("queries-database" (("subpack1" nil (prf) (get-combi get-rotations canon-query sum-lists get-index) nil)
-   ;;         )
-   ;; nil nil nil)
-   )
- (om::find-library "CNMAT-OM")
- )
+(let ((this-lib (om::find-library "CNMAT-OM")))
+  (om::addgenfun2pack '(get-combi get-rotations canon-query sum-lists get-index) this-lib))
 
 ;;; evaluate this to generate the HTML reference
 ;;; (gen-lib-reference (find-library "CNMAT-OM"))
--- CNMAT-OM/src/sum-lists.lisp
+++ CNMAT-OM/src/sum-lists.lisp
@@ -13,22 +13,17 @@
   (list (reduce #' + (flat list))))
 
 ;;;helper function for sum-lists
-(defun reduce-sum (mylist)
-  (reduce #' + (flat mylist))
-)
-
-(defun reduce-sum-lists (mylist)
-  (list (reduce #' + (flat mylist)))
-)
+(defun reduce-sum (list)
+  (reduce #' + (flat list)))
 
 ;;;(defun sum-of-lists (list)
 ;;;  (mapcar #'reduce-sum list))
 
 ;;; output sums of lists of lists
-(om::defmethod! sum-lists ((my-list list) &optional (mode 0))
+(om::defmethod! sum-lists ((my-list list) &optional mode)
   :icon 5
   :indoc '("a list of lists to be summed")
-  :initvals '(((1 2 3) (4 5 6)) 0)
+  :initvals '((1 2 3) (4 5 6))
   :menuins '((1 (("sum of the list of lists per voice" 0) ("sums that preserve list structure" 1))))
   :doc "Returns a list of sums of list arguments"
 
@@ -36,6 +31,6 @@
     ;;;sum of the lists of lists per voice
     (0 (mapcar #'reduce-sum my-list))
     ;;;sum that preserves the list structure
-    (1 (mapcar #'reduce-sum-lists my-list))
+    (1  (flat (mapcar #'map-lists my-list) 1))
     )
 )
