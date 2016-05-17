b25ca70871f362922f4a0f6634bd0d85eaf86b5b
mattschumaker <matt.schumaker@gmail.com>
2016-05-16 22:32:39 -0700

    Revert "Revert "Adding operations""
    This reverts commit 092ed0ee3599a77d69fa1d2d5721bb207876d382.

--- CNMAT-OM/cnmat-om.lisp
+++ CNMAT-OM/cnmat-om.lisp
@@ -10,7 +10,13 @@
    :canon-query 
    :rotation-canon-query
    :sum-lists
-   :get-index))
+   :get-index
+   :count-truncate
+   :cribs
+   :equal-probability
+   :pitch-inversion
+   :repeat-list
+   :retrograde-list))
 
 (in-package :om)
 
@@ -18,10 +24,22 @@
 (compile&load (om-relative-path '("src") "combinations"))
 (compile&load (om-relative-path '("src") "sum-lists"))
 (compile&load (om-relative-path '("src") "get-index"))
+(compile&load (om-relative-path '("src") "count-truncate"))
+(compile&load (om-relative-path '("src") "cribs"))
+(compile&load (om-relative-path '("src") "equal-probability"))
+(compile&load (om-relative-path '("src") "pitch-inversion"))
+(compile&load (om-relative-path '("src") "repeat-list"))
+(compile&load (om-relative-path '("src") "retrograde-list"))
 
 
 
-(set-lib-release 0.1)
+
+
+
+
+
+
+(set-lib-release 0.1 (om::find-library "CNMAT-OM"))
 
 (doc-library "CNMAT-OM contains compositional tools collected from CNMAT researh and projects.")
 
@@ -40,8 +58,23 @@
 
 (in-package :cnmat)
 
-(let ((this-lib (om::find-library "CNMAT-OM")))
-  (om::addgenfun2pack '(get-combi get-rotations canon-query sum-lists get-index) this-lib))
+;(let ((this-lib (om::find-library "CNMAT-OM"))
+;  (om::addgenfun2pack '(get-combi get-rotations canon-query sum-lists get-index) this-lib))
+
+; ("sub-pack-name" subpacke-lists class-list function-list class-alias-list)
+(om::fill-library  
+ '((NIL nil (prf) (get-combi get-rotations canon-query) nil)
+   ("operations" nil (prf) (sum-lists get-index count-truncate cribs equal-probability pitch-inversion repeat-list retrograde-list) nil)
+   ("queries-database" nil (prf) (get-combi get-rotations canon-query) nil)
+
+   
+   ;;use lines below for sub-sub menus, if needed
+   ;;("queries-database" (("subpack1" nil (prf) (get-combi get-rotations canon-query sum-lists get-index) nil)
+   ;;         )
+   ;; nil nil nil)
+   )
+ (om::find-library "CNMAT-OM")
+ )
 
 ;;; evaluate this to generate the HTML reference
 ;;; (gen-lib-reference (find-library "CNMAT-OM"))
