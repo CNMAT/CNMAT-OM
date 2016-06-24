e2e6bbca5d0692d830c9d316b7d83e1ba501b7ae
mattschumaker <matt.schumaker@gmail.com>
2016-06-24 14:41:13 -0700

    New object poly-player-cuts2
    Adding a new object called poly-player-cuts2.  This is a “cut-in” style
    player that allows for distinct tatum lists for each voice.
    Poly-player-cuts2 works in sustain mode only. An example patch called
    “3g poly-player-cuts2.omp” is also included.

--- CNMAT-OM/cnmat-om.lisp
+++ CNMAT-OM/cnmat-om.lisp
@@ -30,7 +30,8 @@
    :poly-player-cuts
    :r-interleave
    :poly->voice
-   :pitch-mapper2))
+   :pitch-mapper2
+   :poly-player-cuts2))
 
 (in-package :om)
 
@@ -58,6 +59,8 @@
 (compile&load (om-relative-path '("src") "r-interleave"))
 (compile&load (om-relative-path '("src") "poly->voice"))
 (compile&load (om-relative-path '("src") "pitch-mapper2"))
+(compile&load (om-relative-path '("src") "poly-player-cuts2"))
+
 
 
 
@@ -95,7 +98,7 @@
 ; ("sub-pack-name" subpacke-lists class-list function-list class-alias-list)
 (om::fill-library  
  '((NIL nil (prf) (get-combi get-rotations canon-query ) nil)
-   ("operations" nil (prf) (sum-lists get-index count-truncate cribs equal-probability pitch-inversion repeat-list retrograde-list rotations->poly rotations->voice rotations->poly2 pitch-mapper1 retrograde-canon-test pitch->bands-collection pitch->bands-pitchclass make-bands poly-player-cuts poly->voice pitch-mapper2) nil)
+   ("operations" nil (prf) (sum-lists get-index count-truncate cribs equal-probability pitch-inversion repeat-list retrograde-list rotations->poly rotations->voice rotations->poly2 pitch-mapper1 retrograde-canon-test pitch->bands-collection pitch->bands-pitchclass make-bands poly-player-cuts poly->voice pitch-mapper2 poly-player-cuts2) nil)
    ("queries-database" nil (prf) (get-combi get-rotations canon-query permute+remove-duplicate-rotations canon-permutations-no-overlaps r-interleave) nil)
 
    
---
+++ CNMAT-OM/src/poly-player-cuts2.lisp
@@ -0,0 +1,168 @@
+;;;==================================
+;;; POLY-PLAYER-CUTS2
+;;;==================================
+
+(in-package :cnmat)
+
+
+(defun cuts-rests (duration tatum)
+
+     (flat (list tatum (om* (om- duration tatum) -1)))
+
+)
+
+;;;;
+;;;; RESTS VERSION DOES NOT WORK YET.  HOW TO GET THE CURRENT TATUM AT THE ATTACK POINT?
+;;;;
+
+(defun make-voice-cuts-rests2 (meter durations-list tatum new-pitches tempo generated-rhythms generated-tatums)
+  (let* ((flat-pitches (flat new-pitches))
+         (cuts-pitches (remove 'nil (posn-match flat-pitches (dx->x 0 durations-list))))
+         (cuts-events (mapcar (lambda (x) (cuts-rests x tatum)) (om* durations-list tatum))))
+         ;;(generated-rhythms-with-rests (flat (mapcar (lambda (x) (cuts-rests x tatum)) generated-rhythms)))
+    ;;return a list with both voices
+       (list
+    ;;this voice is the running version
+         (make-instance 'voice 
+                   :tree (mktree generated-tatums meter)
+                   :chords flat-pitches 
+                   :tempo tempo 
+                   )
+    ;;; this voice is the cut in
+
+          (make-instance 'voice 
+                   :tree (mktree generated-rhythms-with-rests meter)
+                   :chords cuts-pitches 
+                   :tempo tempo 
+                   )
+       
+        )
+    )
+)
+
+
+
+(defun make-voice-cuts2 (meter durations-list tatum new-pitches tempo generated-rhythms generated-tatums)
+  (let* ((flat-pitches (flat new-pitches))
+         (cuts-pitches (remove 'nil (posn-match flat-pitches (dx->x 0 durations-list)))))
+    ;;return a list with both voices
+       (list
+    ;;this voice is the running version
+         (make-instance 'voice 
+                   :tree (mktree generated-tatums meter)
+                   :chords flat-pitches 
+                   :tempo tempo 
+                   )
+    ;;; this voice is the cut in
+
+          (make-instance 'voice 
+                   :tree (mktree generated-rhythms meter)
+                   :chords cuts-pitches 
+                   :tempo tempo 
+                   )
+       
+        )
+    )
+)
+
+(defun check-pitchlist-vs-rhythmlist (rhythm-list pitch-list)
+
+;;For each voice, if the sum of rhythm-list > len(pitchlist) then repeat the pitchlist until it
+;;fills the length of the rhythm list
+
+;;output new pitchlist for the processing
+
+  (let* ((sum-rhythms (reduce '+ rhythm-list))
+        (num-events (om-round (om/ sum-rhythms (length (flat pitch-list))))))
+    
+        (flat (repeat-n (flat pitch-list) num-events))
+   )
+
+)
+
+(defun prep-tatum-helper (tatum-list-elem)
+
+  (let* (( elem (flat tatum-list-elem))
+         (num-times  (first elem))
+         (self (repeat-n (om/ 1 (second elem)) (om/ (second elem) 4))))  
+    
+  (flat (repeat-n self num-times ))
+   )
+)
+
+
+(defun prep-tatum (tatum-list)
+;; map through each voice
+     (flat (mapcar (lambda (x) (prep-tatum-helper x)) tatum-list))
+
+)
+
+(defun generate-rhythms (durations prepped-tatums)
+  (let* (( n-times (ceiling  (reduce '+ (flat durations)) (length prepped-tatums)))
+         (self prepped-tatums)
+         (list-to-group (flat (repeat-n self n-times)))
+         (grouped-list (group-list list-to-group (flat durations) 'linear)))
+
+    (mapcar (lambda (x) (reduce '+ x)) grouped-list)
+
+  )
+)
+
+(defun generate-tatums (durations prepped-tatums)
+  (let* (( n-times (ceiling  (reduce '+ (flat durations)) (length prepped-tatums)))
+         (self prepped-tatums))
+        
+    (flat (repeat-n self n-times))
+
+  
+
+  )
+)
+  
+;;;this receieves list of durations lists (output from get-rotations)
+(om::defmethod! poly-player-cuts2 ( (durations-list list) (meter list) (tatum-list list) (pitches list) (tempo integer) &optional (mode 0))
+
+  :icon 2
+  :indoc '("a list for the meter" "a list of lists for durations" "list of lists of tatums for each voice" "a list of lists of pitches" "a tempo as integer" "mode: 0 = sustain mode output; 1 = rests mode output")
+  :outdoc '("a poly") 
+  :initvals '( ((1 5 7 10)) (4 4) (((1 (16)))) ((6100)) 110 0)
+  :doc "Converts a rotation list into music notation using the ."
+  
+  (let* (( flat-rhythm-lists (mapcar (lambda (x) (flat x)) durations-list))
+        (flat-pitch-lists (mapcar (lambda (x) (flat x)) pitches))
+        (prepped-pitches (mapcar (lambda (x y) (check-pitchlist-vs-rhythmlist x y)) flat-rhythm-lists flat-pitch-lists))
+        (prepped-tatums (mapcar (lambda (x) (prep-tatum x)) tatum-list))
+        (generated-rhythms (mapcar (lambda (x y) (generate-rhythms x y)) durations-list prepped-tatums))
+        (generated-tatums (mapcar (lambda (x y) (generate-tatums x y)) durations-list prepped-tatums)))
+
+    (print flat-rhythm-lists)
+    (print flat-pitch-lists)
+    (print prepped-pitches)
+    (print generated-tatums)
+    (print generated-rhythms)
+
+    
+  (case mode
+
+    (0 (flat (mapcar (lambda (x y z a) (make-voice-cuts2 meter x tatum-list y tempo z a)) flat-rhythm-lists  prepped-pitches generated-rhythms generated-tatums) 1)
+       )
+    (1 (flat (mapcar (lambda (x y z a) (make-voice-cuts-rests2 meter x tatum-list y tempo z a)) flat-rhythm-lists  prepped-pitches generated-rhythms generated-tatums) 1)
+       )
+       
+
+   ;; (2 (print pitches)
+      ;; )
+    )
+  )
+  
+
+)
+
+
+;;;this receives from rfi object
+(om::defmethod! poly-player-cuts2 ( (durations-list prf) (meter list) (tatum list) (pitches list) (tempo integer) &optional (mode 0))
+  (let ((durations (mapcar 'pulses (voices durations-list))))
+    (make-voice-cuts durations meter tatum pitches tempo mode)))
+
+;;;make mode 1 2 3 for sustain, rest, pulses
+;;;
