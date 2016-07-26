b977fa04def7441ce6df839b7f7db23744964f2f
Jean Bresson <bresson@ircam.fr>
2016-07-26 13:07:06 -0700

    prf-editor

--- CNMAT-OM/src/prf-editor.lisp
+++ CNMAT-OM/src/prf-editor.lisp
@@ -23,11 +23,7 @@
 
 (defmethod om::update-editor-after-eval ((self prfeditor) val)
   (setf (om::object self) val)
-  (oa::om-remove-subviews self (datapanel self))
-  (init-prf-editor self)
-  (setf (selection self) nil)
-  (init-selection (datapanel self))
-  (om::update-subviews self))
+  (reset-contents self))
 
 (defmethod init-selection ((self prf-panel))
   (loop for sp in (oa::om-subviews self) do
@@ -35,13 +31,24 @@
         (oa::om-set-bg-color sp oa::*om-white-color*)
         (init-selection sp)))
 
+(defmethod init-selection ((self prfeditor))
+  (setf (selection self) nil)
+  (init-selection (datapanel self)))
+  
+
 (defmethod set-selection ((self prfeditor) (panel prf-panel))
   (init-selection (datapanel self))
-  (setf (selected panel) t)
-  (setf (selection self) panel)  
-  (oa::om-set-bg-color panel (oa::om-make-color 0.9 1 0.9))
-  (oa::om-invalidate-view self))
+  (unless (equal panel (datapanel self))
+    (setf (selected panel) t)
+    (setf (selection self) panel)  
+    (oa::om-set-bg-color panel (oa::om-make-color 0.9 1 0.9))
+    (oa::om-invalidate-view self)))
 
+(defmethod reset-contents ((self prfeditor))
+  (oa::om-remove-subviews self (datapanel self))
+  (init-prf-editor self)
+  (init-selection self)
+  (om::update-subviews self))
 
 (defmethod init-prf-editor ((self prfeditor))
   (oa::om-add-subviews self (setf (datapanel self) 
@@ -93,7 +100,7 @@
            (pos space))
       (loop for sv in (oa::om-subviews self) 
             for i = 0 then (+ i 1) do
-            (let ((nsv (max 1 (length (oa::om-subviews sv)))))
+            (let ((nsv (max 1 (total-subviews sv))))
               (oa::om-set-view-size sv (oa::om-make-point vw (* vh nsv)))
               (oa::om-set-view-position sv (oa::om-make-point vx pos))
               (setf pos (+ pos (* vh nsv) space))
@@ -101,25 +108,88 @@
       )))
   
 ;; not very nice
-(defmethod get-container-r-frame-size ((self prf-panel))
-  (unless (oa::om-subviews self)
-    (get-r-frame-size (prf self))
-    (get-r-frame-size (prf (oa::om-view-container self)))))
+(defmethod get-prf ((self prf-panel))
+  (if (oa::om-subviews self)
+      (prf self)
+    (get-prf (oa::om-view-container self))))
     
- 
+(defmethod get-prf ((self prfeditor))
+  (om::object self))
+    
+
 (defmethod oa::om-draw-contents ((self prf-panel)) 
-  (unless (oa::om-subviews self)
-    (oa::om-with-focused-view 
-        self
-      ;(oa::om-with-font oa::*om-default-font1*
-      ;                  (oa::om-draw-string 4 (- (om::h self) 12) (format nil "PRF: ~A" (prf  self))))
+  (if (oa::om-subviews self)
+    ;(oa::om-with-focused-view self
+    ;  (oa:om-draw-rect-outline 1 1 (- (om::w self) 2) (- (om::h self) 2)))
+    NIL 
+    (oa::om-with-focused-view self
       (draw-rhythmic-line (prf self) 
-                          (get-container-r-frame-size self) 
+                          (get-r-frame-size (get-prf self))
                           4 (- (om::w self) 8) 2 (- (om::h self) 4) 0)  
       )))
 
 (defmethod oa::om-view-click-handler ((self prf-panel) pos)
   (unless (selected self)
     (set-selection (om::editor self) self)))
+
+(defmethod delete-prf-voice ((voice t) (from rhythmic-frame) top-level) nil)
+
+(defmethod delete-prf-voice ((voice t) (from polyrhythmic-frame) top-level)
+  (unless (equal voice from)
+    (if (find voice (voices from))
+        (setf (voices from) (remove voice (voices from)))
+      (loop for v in (voices from) do
+            (delete-prf-voice voice v top-level)))
+    (when (null (voices from)) (delete-prf-voice from top-level top-level))))
+
+
+(defmethod apply-r-substitute ((container-prf polyrhythmic-frame) frame val subs)
+  (let* ((value (or val (ignore-errors 
+                          (read-from-string 
+                           (oa:om-get-user-string "Value to substitute")))))
+         (substitutes (and value 
+                           (or subs 
+                               (ignore-errors 
+                                 (read-from-string 
+                                  (oa:om-get-user-string "Values to substitute with" :initial-string "( )")))))))
+    
+    (when (and value substitutes)
+      
+      (let ((new-frame (r-substitute frame value substitutes))
+            (pos (or (position frame (voices container-prf)) -1)))
+        
+        (setf (voices container-prf) 
+              (insert-in-list (voices container-prf) new-frame (1+ pos)))
+        
+        ))
+    ))
+
+       
+(defmethod om::handle-key-event ((self prfeditor) char)
+  (let ((selected-panel (selection self)))
+    (when selected-panel
+      (let ((selected-frame (prf selected-panel))
+            (container-prf (get-prf selected-panel)))
+        (case char
+          (:om-key-delete 
+           (delete-prf-voice selected-frame (om::object self) (om::object self))
+           (reset-contents self)
+           (om::report-modifications self))
+          (:om-key-esc
+           (init-selection self))
+          (#\s  
+           (apply-r-substitute container-prf selected-frame nil nil)
+           (reset-contents self)
+           (om::report-modifications self))
+          )
+        ))))
+
+(defmethod om::get-menubar ((self prfeditor)) 
+  (list (oa::om-make-menu 
+         "File" 
+         (list 
+          (oa::om-new-leafmenu "Close" #'(lambda () (oa::om-close-window (oa::om-view-window self))) "w")
+          (oa::om-new-leafmenu "Print" #'(lambda () (oa::om-print-window (oa::om-view-window self))) "p")))
+        (om::make-om-menu 'om::windows :editor self)))
 
 
